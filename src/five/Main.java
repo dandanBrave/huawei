48b9df97d2b95236081a09bb2a85b775120d87fd
dandanBrave <269045975@qq.com>
2017-06-29 21:34:18 +0800

    提交华为机试第五道题

---
+++ src/five/Main.java
@@ -0,0 +1,15 @@
+package five;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            String numStr = sc.next().substring(2);
+            System.out.println(Integer.parseInt(numStr,16));
+        }
+    }
+}
