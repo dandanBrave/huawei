3335a01463074e175630e8e6a3f40270acf9fef2
dandanBrave <269045975@qq.com>
2017-06-30 11:46:20 +0800

    提交华为编程第九道题

--- src/nine/Main.java
+++ src/nine/Main.java
@@ -8,7 +8,6 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 import java.util.Set;
-
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
